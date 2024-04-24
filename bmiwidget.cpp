c447244b5e80d44e520e22c5c0f554365bdb86c1
Pranav N Rao <pranavanrao@gmail.com>
2024-04-24 09:34:09 +0530

    Add signals and slots

--- bmiwidget.cpp
+++ bmiwidget.cpp
@@ -1,6 +1,8 @@
 #include "bmiwidget.h"
 #include "ui_bmiwidget.h"
 
+#include <QtGlobal>
+
 BMIWidget::BMIWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::BMIWidget)
@@ -8,7 +10,19 @@ BMIWidget::BMIWidget(QWidget *parent)
     ui->setupUi(this);
     ui->spinBox_height->setValue(0);
     ui->spinBox_height->setMaximum(1000);
-    ui->lable_height->setText("QWERTY");
+    ui->lable_height->setText("Enter Height in cm");
+    ui->label_weight->setText("Enter weight in kgs");
+    ui->label_bmi->setText("BMI result");
+
+    // connect(ui->spinBox_height,
+    //         qOverload<int>(&QSpinBox::valueChanged),
+    //         ui->lcdNumber_bmi,
+    //         qOverload<int>(&QLCDNumber::display));
+
+    connect(ui->spinBox_height,
+            SIGNAL(valueChanged(int)),
+            ui->lcdNumber_bmi,
+            SLOT(display(int)));
 }
 
 BMIWidget::~BMIWidget()
